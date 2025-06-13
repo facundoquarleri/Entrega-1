e778aefaff65032ea9cc580a90f2ba3da66c9150
Facundo Quarleri <facujquarleri@gmail.com>
2025-06-12 23:40:11 -0300

    modelo cliente

--- tp3/migrations/0001_initial.py
+++ tp3/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Alumno',
+            name='Cliente',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('nombre', models.CharField(max_length=50)),
